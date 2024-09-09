0a822ea30831a757c24f76e3a7227eeab11298ff
Anuchit Chalothorn <anoochit@gmail.com>
2024-09-09 12:33:35 +0700

    update myapp key

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@ import 'package:akomap/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
